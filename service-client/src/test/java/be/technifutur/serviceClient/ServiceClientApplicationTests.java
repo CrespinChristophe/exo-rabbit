adc1cf8df2b8fa5bfee0cc8d538619edfcab1d11
Crespin <crespin.christophe@netc.eu>
2022-05-04 10:02:09 +0200

    First commit

---
+++ service-client/src/test/java/be/technifutur/serviceClient/ServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.serviceClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-facturation/src/test/java/be/technifutur/serviceFacturation/ServiceFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.serviceFacturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
